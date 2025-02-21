809e410da5de665c38b34f05334b993bcb2d5da6
Flavio Pontes <flaviocpontes@gmail.com>
2025-02-21 08:32:38 -0300

    TestPrimeFactorsOf4Is2_2 Passing

--- prime_factors.go
+++ prime_factors.go
@@ -3,6 +3,12 @@ package go_prime_factors
 func PrimeFactors(n int) []int {
 	result := []int{}
 	if n > 1 {
+		if n%2 == 0 {
+			result = append(result, 2)
+			n /= 2
+		}
+	}
+	if n > 1 {
 		result = append(result, n)
 	}
 	return result
