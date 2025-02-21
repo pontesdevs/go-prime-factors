82222875ada24de7cb5ef0e525cffb2a80947003
Flavio Pontes <flaviocpontes@gmail.com>
2025-02-21 08:02:18 -0300

    TestPrimeFactorsOf2Is2 Failing

--- prime_factors_test.go
+++ prime_factors_test.go
@@ -13,3 +13,11 @@ func TestPrimeFactorsOf1IsEmptySlice(t *testing.T) {
 		t.Errorf("PrimeFactorsOf1 should be %v but we got %v", want, got)
 	}
 }
+
+func TestPrimeFactorsOf2Is2(t *testing.T) {
+	want := []int{2}
+	got := PrimeFactors(2)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("PrimeFactorsOf1 should be %v but we got %v", want, got)
+	}
+}
