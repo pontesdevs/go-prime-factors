708dcbce02b4c717baaddf86623db06385550836
Flavio Pontes <flaviocpontes@gmail.com>
2025-02-21 09:31:25 -0300

    TestPrimeFactorsOf9Is3_3 Passing

--- prime_factors.go
+++ prime_factors.go
@@ -3,17 +3,12 @@ package go_prime_factors
 func PrimeFactors(n int) []int {
 	result := []int{}
 	if n > 1 {
-		for n%2 == 0 {
-			result = append(result, 2)
-			n /= 2
+		for divisor := 2; divisor <= n; divisor++ {
+			for n%2 == 0 {
+				result = append(result, 2)
+				n /= 2
+			}
 		}
-		for n%3 == 0 {
-			result = append(result, 3)
-			n /= 3
-		}
-	}
-	if n > 1 {
-		result = append(result, n)
 	}
 	return result
 }
